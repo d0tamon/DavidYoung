ca5b84c705377bebf765294b5422d76981a827b7
d0tamon <d0tamon@icloud.com>
2020-03-02 14:11:59 -0700

    Update Encrypting.py

--- Encrypting.py
+++ Encrypting.py
@@ -4,23 +4,30 @@ Created on Tue Nov 26 12:45:44 2019
 
 @author: SahilPatil
 """
+#Import Libraries
 from getpass import getpass as GP
 from cryptography.fernet import Fernet
 import os
 
+#Create Credential Variables
 credential = {'username': None,
               'password':None}
-
+#Input Username
 credential['username'] = input("Enter your username: ")
+
+#Input Password
 password = GP("Enter your password: ")
 
+#Encode Password
 password_byte = password.encode('utf-8')
+
+#Generate Key and Token
 key = Fernet.generate_key()
 cipher = Fernet(key)
 token = cipher.encrypt(password_byte)
 
 
-
+#Change Directory of where to save the password files
 os.chdir(r"C:\Users\Username\Desktop\Python Files\Passwords")
 with open("key.txt", "wb") as file:
     file.write(key)
